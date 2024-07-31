35f1cf5ba1fc79d120dff595b7e1bfcb87feea46
AliAlTaweel <engalihaitham@gmail.com>
2024-07-31 10:40:49 +0300

    v2

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-let adviceText = document.getElementsByClassName("advice");
+let adviceText = document.querySelector(".advice");
+let adviceNumber = document.querySelector(".main h3");
+let clickedImg = document.querySelector(".dais img");
 
-console.log(adviceText);
+
+async function func(event) {
+  try {
+    let response = await fetch("https://api.adviceslip.com/advice");
+  
+    if (!response.ok) {
+      throw new Error("Website API response notOk");
+    }
+    let jsonData = await response.json();
+    adviceText.textContent = `"${jsonData.slip.advice}"`;
+    adviceNumber.textContent = `Advice #${jsonData.slip.id}`;
+  } catch (error) {
+    console.error("error catch");
+  }
+}
+clickedImg.addEventListener("click", func);
